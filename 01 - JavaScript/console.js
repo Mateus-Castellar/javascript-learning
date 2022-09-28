4a03a49b4f2a7859b48760ba92130584b77930ca
Mateus Castellar <mateuscastellar@outlook.com>
2022-09-28 14:06:39 -0300

    feat: Console com javaScript

---
+++ 01 - JavaScript/console.js
@@ -0,0 +1,12 @@
+console.log("usando console no javascript");
+console.log('pode usar aspas simples');
+console.log("pode usar aspas duplas");
+console.log("aspas duplas permite printar aspas simples dentro do console ''");
+
+let texto = "sou uma variável";
+console.log(`crases são usadas para a interpolação de strings: ${texto}`);
+
+console.log("podemos printar números também")
+console.log(12345);
+
+console.log("no js nossos dados basicos serão: Number/String")
